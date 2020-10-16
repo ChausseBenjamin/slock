b5c81157067c256f16242b7488b1112e8fa13175
Benjamin Chausse <benjamin@chausse.xyz>
2020-10-16 00:10:05 -0400

    Apply patches

--- config.def.h
+++ config.def.h
@@ -1,12 +1,18 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#66aabb",   /* during input */
+	[FAILED] = "#cc3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
